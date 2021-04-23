a5811d132ad48241bd767441b167d8d85062c40a
ganler <jaway.liu@gmail.com>
2021-04-23 23:31:27 +0800

    feat: acceleration via mirror
    feat: conditional install neovim
    
    fix: backtrace

--- init.sh
+++ init.sh
@@ -4,15 +4,17 @@ set -e
 echo -e "\e[92mIf you found your github connection too slow (possibly you are in CHINA!), please use a mirror website:\e[0m"
 echo -e '\e[92mgit config --global url."https://hub.fastgit.org/".insteadOf "https://github.com/"\e[0m'
 
+[ $CHINA == 1 ] && export ACCELERATE_PREFIX=https://ghproxy.com/ && echo -e "\e[92mUsing mirror: $ACCELERATE_PREFIX\e[0m"
+
 # neovim.py
 python3 -m pip install neovim
 
 # NodeJS
 NODE_TAR_GZ=node-v15.9.0-linux-x64.tar.gz
-which npm || (wget --no-clobber https://nodejs.org/download/release/v15.9.0/$NODE_TAR_GZ && tar xf $NODE_TAR_GZ && mv node-v15.9.0-linux-x64 node && rm $NODE_TAR_GZ && echo "export PATH=\$PATH:$(realpath node)/bin")
+which npm || (wget https://nodejs.org/download/release/v15.9.0/$NODE_TAR_GZ && tar xf $NODE_TAR_GZ && mv node-v15.9.0-linux-x64 node && rm $NODE_TAR_GZ && echo "export PATH=\$PATH:$(realpath node)/bin")
 
 # neovim
-which nvim || (wget --no-clobber https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && chmod u+x nvim.appimage && mkdir -p ~/bin && mv nvim.appimage ~/bin/nvim)
+which nvim || (wget ${ACCELERATE_PREFIX}https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && chmod u+x nvim.appimage && mkdir -p ~/bin && mv nvim.appimage ~/bin/nvim)
 
 # vim-plug
 [ -e $HOME/.local/share/nvim/site/autoload/plug.vim ] || sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
